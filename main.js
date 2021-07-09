956b82454b96df15e91aad26c770b58937043ed7
Rishisankhe04 <86875042+Rishisankhe04@users.noreply.github.com>
2021-07-09 13:56:03 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function play1() {
-    document.getElementById("audio1").pause();
-    document.getElementById("audio1").play();
-}
-
-function play2() {
-    document.getElementById("audio1").pause();
-    document.getElementById("audio2").play();
-}
