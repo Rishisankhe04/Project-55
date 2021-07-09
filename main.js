20f4044f3d60857794248b9a837bacf30165fc16
Rishisankhe04 <86875042+Rishisankhe04@users.noreply.github.com>
2021-07-09 13:22:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function play1() {
+    document.getElementById("audio1").pause();
+    document.getElementById("audio1").play();
+}
+
+function play2() {
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").play();
+}
